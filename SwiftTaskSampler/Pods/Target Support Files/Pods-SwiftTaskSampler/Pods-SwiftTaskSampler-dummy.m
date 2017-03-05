9ba6db04a3738549ea3e40cd8e2680eb4e6e9b1c
shoheiyokoyama <shohei.yok0602@gmail.com>
2017-03-05 11:01:18 +0900

    Create SwiftTaskSampler

---
+++ SwiftTaskSampler/Pods/Target Support Files/Pods-SwiftTaskSampler/Pods-SwiftTaskSampler-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftTaskSampler : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftTaskSampler
+@end
---
+++ SwiftTaskSampler/SwiftTaskSampler/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftTaskSampler
+//
+//  Created by 横山祥平 on 2017/03/04.
+//  Copyright © 2017年 Shohei. All rights reserved.
+//
+
+import UIKit
+import SwiftTask
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
