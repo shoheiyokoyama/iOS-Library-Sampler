851b5a9ca356c8cf0ab7002706e74c04e01ae6fd
shoheiyokoyama <shohei.yok0602@gmail.com>
2017-04-26 08:57:45 +0900

    Use sample

--- JTSImageViewControllerSampler/JTSImageViewControllerSampler/ViewController.swift
+++ JTSImageViewControllerSampler/JTSImageViewControllerSampler/ViewController.swift
@@ -7,19 +7,27 @@
 //
 
 import UIKit
+import JTSImageViewController
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func tapButton(_ sender: Any) {
+        
+        let info = JTSImageInfo()
+        info.image = imageView.image
+        info.referenceView = imageView.superview
+        info.referenceRect = imageView.frame
+        info.referenceContentMode = imageView.contentMode
+        
+        let viewer = JTSImageViewController(imageInfo: info, mode: JTSImageViewControllerMode.image, backgroundStyle: JTSImageViewControllerBackgroundOptions.blurred)
+        viewer?.show(from: self, transition: JTSImageViewControllerTransition.fromOriginalPosition)
     }
-
-
 }
 
